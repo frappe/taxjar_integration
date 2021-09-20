2d840b23e74a89e81d0300c27a02688f2bb14fe1
Subin Tom <36098155+nemesis189@users.noreply.github.com>
2021-09-20 16:13:36 +0530

    fix: Tax Breakup table headers fix (#27596)

---
+++ taxjar_integration/erpnext/erpnext_integrations/data_migration_mapping/milestone_to_project/__init__.py
@@ -0,0 +1,9 @@
+from __future__ import unicode_literals
+
+
+def pre_process(milestone):
+	return {
+		'title': milestone.title,
+		'description': milestone.description,
+		'state': milestone.state.title()
+	}
